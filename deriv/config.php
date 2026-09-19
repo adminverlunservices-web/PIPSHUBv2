69e4e72c43f825dc9bd1d550a089d9f98a1f5367
adminverlunservices-web <admin.verlun.services@gmail.com>
2026-09-19 13:50:08 +0300

    Update config.php

--- deriv/config.php
+++ deriv/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('CLIENT_ID', '019e7e0d-b4ad-7d46-be70-ed7f34e51224');
-define('REDIRECT_URI', 'https://customerrelations.online/PIPSHUBv2/deriv/callback.php');
+define('CLIENT_ID', '33Qn9yJLubVP6VQ8F5MoG');
+define('REDIRECT_URI', 'https://customerrelations.online/deriv/callback.php');
 define('AUTH_URL', 'https://auth.deriv.com/oauth2/auth');
 define('TOKEN_URL', 'https://auth.deriv.com/oauth2/token');
